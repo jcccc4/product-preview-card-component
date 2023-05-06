fc3aa0085897ba15c8388a4667edea2ab4a1f9fe
jcccc4 <jcsacra4@gmail.com>
2023-05-06 17:49:49 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-
-}
-
-module.exports = nextConfig
+module.exports = {
+  // Set the public directory
+  publicRuntimeConfig: {
+    staticFolder: '/static',
+  },
+};
